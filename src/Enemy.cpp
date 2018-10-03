a9f6c9d29bdd46fe328e62bbef70df6e24dcc3a4
ZhishengXu <zhx22@pitt.edu>
2018-10-03 11:03:50 -0400

    addHeaderForMac

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,6 +1,11 @@
-
+// checking for apple, else everyone else
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
 #include <SDL.h>
 #include <SDL_image.h>
+#endif
 
 class Enemy
 {
