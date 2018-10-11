1aaf237fab90bfe6e62a009a39ee5677a950c287
Rdereje <rud6@pitt.edu>
2018-10-11 17:48:35 -0400

    my changed

--- src/physics.hpp
+++ src/physics.hpp
@@ -1,59 +1,77 @@
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
 
-int FOR_LIMIT = 3;
-int BACK_LIMIT = -3;
-int NEUTRAL_X_FOR = 0;
-int NEUTRAL_X_BACK = 0;
-int NEUTRAL_Y_UP = 0;
-int NEUTRAL_Y_DOWN = 0;
+class Physics
+{
+
 //the max speed the player can go
-void Speed_Limit(int* x_vel, int *y_vel)
-{
-		// Check speed limits
-
-		if (*x_vel < BACK_LIMIT)
-
-			*x_vel = BACK_LIMIT;
-
-		else if (*x_vel > FOR_LIMIT)
-
-			*x_vel = FOR_LIMIT;
-
-
-
-		if (*y_vel < BACK_LIMIT)
-
-			*y_vel = BACK_LIMIT;
-
-		else if (*y_vel > FOR_LIMIT)
-
-			*y_vel = FOR_LIMIT;
-			return;
-}
+public:
+	Physics(int* x_v, int *y_v, int* x_d, int* y_d,int max_speed, int neutral_pos, int neutral_neg):
+	BACK_LIMIT{-max_speed}, FOR_LIMIT{max_speed},NEUTRAL_X_FOR{neutral_pos},NEUTRAL_X_BACK{neutral_neg},
+NEUTRAL_Y_DOWN{0}, NEUTRAL_Y_UP{0},x_vel{x_v},y_vel{y_v},x_del{x_d}, y_del{y_d}
+	{
+	}
+	void Speed_Limit()
+	{
+			// Check speed limits
+	
+			if (*x_vel < BACK_LIMIT)
+			{
+				*x_vel = BACK_LIMIT;
+			}
+			else if (*x_vel > FOR_LIMIT)
+			{
+				*x_vel = FOR_LIMIT;
+			}
+			if (*y_vel < BACK_LIMIT)
+			{
+				*y_vel = BACK_LIMIT;
+			}
+			else if (*y_vel > FOR_LIMIT)
+			{
+				*y_vel = FOR_LIMIT;
+			}
+	}
 //What to do when no buttons are pressed
-void Neutral(int *x_del, int *y_del, int *x_vel, int *y_vel)
-{
-	if(*x_del == 0)
+	void Neutral()
 	{
-		if(*x_vel > NEUTRAL_X_FOR)
-			{
-				*x_del = -1;
+		if(*x_del == 0)
+		{
+			if(*x_vel > NEUTRAL_X_FOR)
+				{
+					*x_del = -1;
+				}
+				else if (*x_vel < NEUTRAL_X_BACK)
+				{
+					*x_del = 1;
+				}
 			}
-			else if (*x_vel < NEUTRAL_X_BACK)
+			if (*y_del == 0)
 			{
-				*x_del = 1;
+				if (*y_vel > NEUTRAL_Y_UP)
+				{
+					*y_del = -1;
+				}
+				else if (*y_vel < NEUTRAL_Y_DOWN)
+				{
+					*y_del = 1;
+				}
 			}
 	}
-	if (*y_del == 0)
-	{
-		if (*y_vel > NEUTRAL_Y_UP)
-		{
-			*y_del = -1;
-		}
-		else if (*y_vel < NEUTRAL_Y_DOWN)
-		{
-			*y_del = 1;
-		}
-	}
-	return;
-}
-
+	private:
+	int FOR_LIMIT;
+	int BACK_LIMIT;
+	int NEUTRAL_X_FOR;
+	int NEUTRAL_X_BACK;
+	int NEUTRAL_Y_UP;
+	int NEUTRAL_Y_DOWN;
+	int *x_vel;
+	int *y_vel;
+	int *x_del;
+	int *y_del;
+};
