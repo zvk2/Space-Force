9e41062b24e3cfd2b1951048b5244c02fa519dfb
ZhishengXu <zhx22@pitt.edu>
2018-10-17 12:06:55 -0400

    mac ppl

--- src/matrix.h
+++ src/matrix.h
@@ -1,5 +1,11 @@
 #include <stdio.h>
-#include "GL/glew.h"
+#ifdef __APPLE__  // include Mac OS X verions of headers
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#include <OpenGL/gl3.h>
+#else // non-Mac OS X operating systems
+#include <GL/glew.h>
+#endif
 
 typedef struct vec4 {
 	GLfloat x;
