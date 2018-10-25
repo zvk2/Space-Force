2ac708111bcfaffe07821b0e3ebf4ae2e3ebabc2
Rdereje <rud6@pitt.edu>
2018-10-25 16:33:22 -0400

    Update src/Magnetar.cpp

--- src/Magnetar.cpp
+++ src/Magnetar.cpp
@@ -3,6 +3,7 @@ Magnetar::Magnetar(Player* main, SDL_Texture* gIm):ply(main), im(gIm)
 {
 	gRenderer = ply->getRend();
 	collBox = {0,0,1280,721};
+	//gets a pointer to the player's camBox
 	playerCam = ply->getPlayerCamLoc();
 	camBox = {1280,0,1280,720};
 	onScreen = false;
@@ -10,18 +11,24 @@ Magnetar::Magnetar(Player* main, SDL_Texture* gIm):ply(main), im(gIm)
 }
 void Magnetar::Render()
 {	
+	//animation
 	if(frame >= 9)
 		frame = 0;
 	collBox.y = (frame/3)*721;
 	frame++;
+	//places object to screen
 	SDL_RenderCopy(gRenderer, im, &collBox, &camBox);
 	double ACCEL = ply->getACCEL();
 	bool inter = SDL_HasIntersection(&camBox,playerCam);
 	if ((inter && ACCEL > 0) || (!inter && ACCEL < 0))
+	//if player has intersected with the object and move is postive it
+	//will make it negative to inverse controls
+	//but if the intersection breaks will make ACCEL postive again
 	{
 		ply->changeAccel(-ACCEL);
 	}
 	camBox.x = camBox.x - 1;
+	//Used to determine how long to call Magnetar for
 	if(camBox.x + 1280> 0)
 	{
 		onScreen = true;
